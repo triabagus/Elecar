1358d7e62e05490ad9bd70ed89ecf1a7a6fb8ad0
triabagus <triatop9@gmail.com>
2022-10-18 23:47:11 +0700

    Popular section

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,26 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== POPULAR SWIPER ===============*/
+let swiper = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grapCursor: true,
 
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+    breakpoints:{
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            spaceBetween: 48,
+        }
+    }
+})
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
